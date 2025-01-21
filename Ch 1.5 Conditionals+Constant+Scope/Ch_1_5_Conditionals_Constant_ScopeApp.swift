f038d781d3b40855621f9f2fdc7799309f8cb075
schmidln <schmidln@bc.edu>
2025-01-20 22:45:15 -0500

    Initial Commit

---
+++ Ch 1.5 Conditionals+Constant+Scope/Ch_1_5_Conditionals_Constant_ScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ch_1_5_Conditionals_Constant_ScopeApp.swift
+//  Ch 1.5 Conditionals+Constant+Scope
+//
+//  Created by Lucas Schmidt on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Ch_1_5_Conditionals_Constant_ScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
